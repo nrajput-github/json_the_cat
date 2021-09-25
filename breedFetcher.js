6e71fcaff3296971404bea877046d55fb3ac5dcd
Navya Rajput <navyasrajput@gmail.com>
2021-09-24 23:51:29 -0400

    Testing Cats as a Service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,22 @@
 const request = require('request');
-let breedName = process.argv[2];
-request(`https://api.thecatapi.com/v1/breeds/search?name=${breedName}`, (error, response, body) => {
-console.log(typeof body);
 
-// console.log(typeof error);
-// console.log(error);
-// const errData = JSON.parse(error); //Convert string to object
-// console.log(errData);
-if (error !== null) {
-console.log(error);
-return error;
-}
-const data = JSON.parse(body); //Convert string to object
-if (!Array.isArray(data) || !data.length ) {
-console.log("Breed Not Found: " + breedName);
-return breedName;
-}
 
-// if (data[0]["name"] !== breedName) {
-// console.log("Invalid cat name: " + breedName);
-// return breedName;
-// }
+const fetchBreedDescription = function(breedName, callback) {
 
-console.log(data);
-console.log(typeof data);
-console.log(data[0]["description"]);
-});
+      request(`https://api.thecatapi.com/v1/breeds/search?name=${breedName}`, (error, response, body) => {
+
+      if (error !== null) {
+      return callback(error);
+
+      }
+      const data = JSON.parse(body); //Convert string to object
+      if (!Array.isArray(data) || !data.length ) {
+      return callback("Breed Not Found: " + breedName);
+
+      }
+
+      return callback(null, data[0]["description"]);
+      });
+};
+
+module.exports = { fetchBreedDescription };
